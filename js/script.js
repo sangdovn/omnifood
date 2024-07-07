ef87629aad100affa785c43a7c958eac7d534e9d
Sang <svngdo@gmail.com>
2024-07-07 19:20:00 +0700

    add js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const header = this.querySelector('.header');
-  const btnNav = this.querySelector('.btn--mobile-nav');
+document.addEventListener("DOMContentLoaded", function () {
+  const header = this.querySelector(".header");
+  const btnNav = this.querySelector(".btn--mobile-nav");
 
-  btnNav.addEventListener('click', function () {
-    header.classList.toggle('nav-open');
-  })
-})
+  btnNav.addEventListener("click", function () {
+    header.classList.toggle("nav-open");
+  });
+
+  // Update year
+  const year = this.querySelector(".year");
+  year.textContent = new Date().getFullYear();
+
+  const sectionHeroEl = document.querySelector(".section-hero");
+
+  const rootMargin = -(window.innerHeight / 1.5)
+
+  const obs = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+      console.log(ent);
+
+      if (ent.isIntersecting === false) {
+        document.body.classList.add("sticky");
+      }
+
+      if (ent.isIntersecting === true) {
+        document.body.classList.remove("sticky");
+      }
+    },
+    {
+      // In the viewport
+      root: null,
+      threshold: 0,
+      rootMargin: `${rootMargin}px`,
+    }
+  );
+  obs.observe(sectionHeroEl);
+});
